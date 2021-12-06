256ddfea2d49cd4b45c44625809f96f70826eecc
Osycle <osycle@gmail.com>
2021-12-06 18:16:57 +0500

    20211206

---
+++ pages/winter-lep.vue
@@ -0,0 +1,22 @@
+<template>
+  <div :class="$style['winter']">
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque distinctio magni iusto aspernatur eaque repellat similique labore deleniti hic nobis dolorem ducimus aliquam ex tempore possimus quisquam ipsa, quis velit!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'winter',
+  computed: {
+    contacts() {
+      return this.$store.state.app.contacts
+    },
+  },
+}
+</script>
+
+<style lang="scss" module>
+  .winter{
+    padding: 2px;
+  }
+</style>
