e66d2efc60c4e81571b7bc25b77018188e0b57ae
Osycle <osycle@gmail.com>
2022-02-22 16:00:04 +0500

    20220222

---
+++ pages/testdrive.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Presentation />
+  </div>
+</template>
+
+
+
+<script>
+
+
+import Presentation from "~/components/layout/Presentation";
+
+export default {
+  created(){
+    this.$cookies.set('verify', true, {
+      maxAge: 60 * 60
+    })
+  },
+  components:{
+    Presentation
+  }
+}
+</script>
