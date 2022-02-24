3f55f6a97f47572387b8df5ced927a18251376a1
Osycle <osycle@gmail.com>
2022-02-24 17:23:10 +0500

    20220224

--- pages/testdrive.vue
+++ pages/testdrive.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <Presentation />
+<style>
+  #verify-block{
+    pointer-events: none;
+    opacity: 0;
+  }
+</style>
   </div>
 </template>
 
@@ -13,12 +19,61 @@ import Presentation from "~/components/layout/Presentation";
 
 export default {
   created(){
-    this.$cookies.set('verify', true, {
-      maxAge: 60 * 60
-    })
+    // this.$cookies.set('verify', true, {
+    //   maxAge: 60 * 60
+    // })
+  },
+  mounted(){
+    // this.$cookies.set('verify', false, {
+    //   maxAge: -1
+    // })
+    
+    function setCookie(name, value, options = {}) {
+
+      options = {
+        path: '/',
+        // при необходимости добавьте другие значения по умолчанию
+        ...options
+      };
+
+      if (options.expires instanceof Date) {
+        options.expires = options.expires.toUTCString();
+      }
+
+      let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+      for (let optionKey in options) {
+        updatedCookie += "; " + optionKey;
+        let optionValue = options[optionKey];
+        if (optionValue !== true) {
+          updatedCookie += "=" + optionValue;
+        }
+      }
+
+      document.cookie = updatedCookie;
+    }
+    function getCookie(name) {
+      let matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+      ));
+      return matches ? decodeURIComponent(matches[1]) : undefined;
+    }
+    function deleteCookie(name) {
+      setCookie(name, "", {
+        'max-age': -1
+      })
+    }
+    // // Пример использования:
+    // setTimeout(() => {
+    //   setCookie('verify', 'false', {secure: true, 'max-age': 9999999, path: "/testdrive"});
+      
+    // }, 100);
+    var vm = this;
+    window.asd = vm;
   },
   components:{
     Presentation
   }
 }
 </script>
+
